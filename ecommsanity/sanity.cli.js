dca7b7d19f55ba22c18d4fa2356090ded78b8032
Sumit <marvelsumit2002@gmail.com>
2023-03-19 15:05:13 +0530

    first commit

---
+++ ecommsanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'wczqyk9s',
+    dataset: 'production'
+  }
+})
---
+++ ecommsanity/schemas/index.js
@@ -0,0 +1,8 @@
+import Banner from "./banner";
+import Product from "./product";
+
+
+export const schemaTypes = [
+    Banner,
+    Product
+]
